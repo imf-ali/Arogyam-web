d0d4a2627abc8aa27fdd760915cd19c8c956613a
Sloka <sawansloka@gmail.com>
2025-01-28 00:04:19 +0530

    added card design in treatment

--- src/components/TreatmentPage/TreatmentItem.js
+++ src/components/TreatmentPage/TreatmentItem.js
@@ -6,22 +6,23 @@ import NotFound from '../NotFound';
 const TreatmentItem = () => {
   const { name } = useParams();
   const treatment = treatmentData.treatments.find((item) => item.path === name);
+
   return (
     <div className={styles.treatmentItem}>
       {treatment ? (
         <>
-         {Object.keys(treatment.data).map(key => (
-           <div key={key}>
-            <h2>{key}</h2>
-            {Array.isArray(treatment.data[key]) ? (
-              <ul>
-                {treatment.data[key].map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              </ul>
+          {Object.keys(treatment.data).map((key) => (
+            <div key={key}>
+              <h2>{key}</h2>
+              {Array.isArray(treatment.data[key]) ? (
+                <ul>
+                  {treatment.data[key].map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
               ) : typeof treatment.data[key] === 'object' ? (
                 <div>
-                  {Object.keys(treatment.data[key]).map(subKey => (
+                  {Object.keys(treatment.data[key]).map((subKey) => (
                     <div key={subKey}>
                       <strong>{subKey}: </strong> {treatment.data[key][subKey]}
                     </div>
@@ -30,14 +31,14 @@ const TreatmentItem = () => {
               ) : (
                 <p>{treatment.data[key]}</p>
               )}
-          </div>
-        ))}
+            </div>
+          ))}
         </>
       ) : (
         <NotFound />
       )}
     </div>
-  )
+  );
 }
 
 export default TreatmentItem;
